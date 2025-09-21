5d21dd9959fe8a912964f329221577fcd276e68e
ioeftedal <ivar.odegardstuen.eftedal@nmbu.no>
2025-09-21 19:44:33 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-25.05;
+    nixpkgs-unstable.url = github:NixOS/nixpkgs/nixos-unstable;
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-unstable,
+    ...
+  }: {
+    
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [./configuration.nix];
+    };
+  };
+}
